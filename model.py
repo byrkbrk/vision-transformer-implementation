07f5a651c235ff78e4892d9c2bca8a0774c1e026
burak <byrkbrk@gmail.com>
2023-03-21 07:18:12 +0300

    add other classes for ViT

--- model.py
+++ model.py
@@ -1,7 +1,19 @@
 import torch
 import torch.nn as nn
 
+
+
 class EmbeddingLayer(nn.Module):
     def __init__(self):
         pass
         # implement here patchify method
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        pass
+
+
+class SelfAttention(nn.Module):
+    def __init__(self):
+        pass
